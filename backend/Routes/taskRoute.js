c3f2da70af47a7d38f2546235ea57ed3dfa7063d
Yong <112306085@g.nccu.edu.tw>
2025-05-24 16:54:32 +0800

    update

--- backend/Routes/taskRoute.js
+++ backend/Routes/taskRoute.js
@@ -17,7 +17,7 @@ const router = express.Router();
 router.get("/normal",getNormalTasks); //定義訪問【一般任務】的路由，將請求發給getNormalTask
 router.get("/top",getTopTasks);//定義訪問【置頂任務】的路由，將請求發給getTopTask
 router.post("/submit",submitTask);//定義訪問【提交任務】的路由，將請求發給submitTask
-router.post("/search",searchTask);
+router.get("/search",searchTask);
 /*router.post("/accept",acceptTask);//定義訪問【接受任務】的路由，將請求發給acceptTask
 router.get("/details/:taskID",getTaskDetails);//定義訪問【任務詳情】的路由，將請求發給getTaskDetails
 router.post("/complete", completeTask);//定義訪問【完成任務】的路由，將請求發給completeTask
