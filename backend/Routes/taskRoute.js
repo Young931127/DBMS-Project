d655df508102c0ad350d9ecb838b81e63d32b663
EdwardLyu <147014804+EdwardLyu@users.noreply.github.com>
2025-05-25 14:07:57 +0800

    detail page

--- backend/Routes/taskRoute.js
+++ backend/Routes/taskRoute.js
@@ -18,6 +18,7 @@ router.get("/normal",getNormalTasks); //定義訪問【一般任務】的路由�
 router.get("/top",getTopTasks);//定義訪問【置頂任務】的路由，將請求發給getTopTask
 router.get("/submit",submitTask);//定義訪問【提交任務】的路由，將請求發給submitTask
 router.get("/search",searchTask);
+router.get("/details/:taskID",getTaskDetails);//定義訪問【任務詳情】的路由，將請求發給getTaskDetails
 /*router.post("/accept",acceptTask);//定義訪問【接受任務】的路由，將請求發給acceptTask
 router.get("/details/:taskID",getTaskDetails);//定義訪問【任務詳情】的路由，將請求發給getTaskDetails
 router.post("/complete", completeTask);//定義訪問【完成任務】的路由，將請求發給completeTask
