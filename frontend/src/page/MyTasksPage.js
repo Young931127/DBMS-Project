3fe359abc801972d3d4c2495505d112ab0511d0a
澪伍 <118983524+zerof0512@users.noreply.github.com>
2025-05-25 15:22:51 +0800

    Update MyTasksPage.js

--- frontend/src/page/MyTasksPage.js
+++ frontend/src/page/MyTasksPage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import ReactPaginate from "react-paginate";
-import { fetchCurrentUser, fetchTasksByUser } from "./api/myTasksApi";
+import { fetchCurrentUser, fetchTasksByUser } from "../api/myTasksApi";
 import "./MyTasksPage.css";
 
 const ITEMS_PER_PAGE = 10;
